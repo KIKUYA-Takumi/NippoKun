495f1679b7a51ca8cebf3929f2dffd57cbae235f
KIKUYA-Takumi <uwasanoace@yahoo.co.jp>
2016-09-21 14:39:21 +0900

    modify file : add class ReportAdmin and ScoreAdmin

--- NippoKun/report/admin.py
+++ NippoKun/report/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import Report, Score
+
 # Register your models here.
+
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'report_author', 'report_title', 'report_content', 'created_at', 'updated_at',)
+
+
+admin.site.register(Report, ReportAdmin)
+
+
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'score_author', 'score', 'evaluate_point', 'comment', 'total_score')
+
+
+admin.site.register(Score, ScoreAdmin)
