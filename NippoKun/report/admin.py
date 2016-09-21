666be86811c82a6355d6c43f550f8fd377607226
KIKUYA-Takumi <uwasanoace@yahoo.co.jp>
2016-09-21 17:32:02 +0900

    modify file : modify template

--- NippoKun/report/admin.py
+++ NippoKun/report/admin.py
@@ -13,7 +13,7 @@ admin.site.register(Report, ReportAdmin)
 
 
 class ScoreAdmin(admin.ModelAdmin):
-    list_display = ('id', 'score_author', 'score', 'evaluate_point', 'comment', 'average_score')
+    list_display = ('id', 'score_author', 'score', 'evaluate_point', 'comment',)
 
 
 admin.site.register(Score, ScoreAdmin)
