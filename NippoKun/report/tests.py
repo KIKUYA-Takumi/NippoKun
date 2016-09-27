6c7fdb15c20951e6e28cb0a092e256415189872d
KIKUYA-Takumi <uwasanoace@yahoo.co.jp>
2016-09-27 12:25:30 +0900

    make test

--- NippoKun/report/tests.py
+++ NippoKun/report/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+
 
 # Create your tests here.
+
+
+class ClientTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_login(self):
+        response = self.client.post('/report/login/')
+        self.assertEqual(response.status_code, 500)
+
+    def test_mypage(self):
+        response = self.client.get('/report/mypage/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_index(self):
+        response = self.client.get('/report/index/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_search(self):
+        response = self.client.get('/report/search/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_register(self):
+        response = self.client.get('/report/user_register/')
+        self.assertEqual(response.status_code, 200)
