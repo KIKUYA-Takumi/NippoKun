77d185e12a0199e2128c91b7c852d8ce2ed32c44
KIKUYA-Takumi <uwasanoace@yahoo.co.jp>
2016-09-28 16:09:35 +0900

    modify test case: Update report's title and content

--- NippoKun/report/tests.py
+++ NippoKun/report/tests.py
@@ -1,29 +1,36 @@
-from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.test import TestCase, Client, RequestFactory
+
+from .models import Report
 
 
 # Create your tests here.
 
 
-class ClientTest(TestCase):
+class UpdateReportTest(TestCase):
     def setUp(self):
         self.client = Client()
+        self.client.post('/report/user_register/',
+                         {'username': 'john',
+                          'password1': 'johnpass',
+                          'password2': 'johnpass'})
+        self.client.login(username='john', password='johnpass')
+        request_factory = RequestFactory()
+        self.request = request_factory.get('/report/mypage/')
 
-    def test_login(self):
-        response = self.client.post('/report/login/')
-        self.assertEqual(response.status_code, 500)
-
-    def test_mypage(self):
-        response = self.client.get('/report/mypage/')
-        self.assertEqual(response.status_code, 302)
-
-    def test_index(self):
-        response = self.client.get('/report/index/')
-        self.assertEqual(response.status_code, 302)
-
-    def test_search(self):
-        response = self.client.get('/report/search/')
-        self.assertEqual(response.status_code, 302)
-
-    def test_user_register(self):
-        response = self.client.get('/report/user_register/')
-        self.assertEqual(response.status_code, 200)
+    def test_update_report_content(self):
+        self.request.user = User.objects.get(pk=1)
+        self.client.post('/report/report_entries/',
+                         {'report_author': self.request.user,
+                          'report_title': 'test title',
+                          'report_content': 'test'
+                          })
+        self.request.report = Report.objects.get(pk=1)
+        report = {
+            'report_author': self.request.report.report_author,
+            'report_title': self.request.report.report_title,
+            'report_content': 'update content'
+        }
+        self.client.post('/report/1/edition/', report)
+        self.request.report = Report.objects.get(pk=1)
+        self.assertEqual(self.request.report.report_content, 'update content')
--- NippoKun/report/urls.py
+++ NippoKun/report/urls.py
@@ -12,20 +12,22 @@ from .views import (
     DeleteReport,
     CreateUser,
     CreateScore,
-    UpdateScore
+    UpdateScore,
+    DeleteScore
 )
 
 urlpatterns = [
-    url(r'^report_entries/', login_required(CreateReport.as_view()), name='create_report'),
     url(r'^search/', login_required(views.search), name='search'),
     url(r'^index/', login_required(ListReport.as_view()), name='index'),
     url(r'^mypage/', login_required(ListMyReport.as_view()), name='mypage'),
+    url(r'^report_entries/', login_required(CreateReport.as_view()), name='create_report'),
     url(r'^(?P<pk>[0-9]+)/$', login_required(DetailReport.as_view()), name='detail_report'),
     url(r'^(?P<pk>[0-9]+)/edition/$', login_required(UpdateReport.as_view()), name='update_report'),
     url(r'^(?P<pk>[0-9]+)/delete/$', login_required(DeleteReport.as_view()), name='delete_report'),
     url(r'^login/', login, {'template_name': 'report/login.html'}, name='login'),
-    url(r'^user_register/$', CreateUser.as_view(), name='create_user'),
     url(r'^logout/$', logout, {'template_name': 'report/logout.html'}, name='logout'),
+    url(r'^user_register/$', CreateUser.as_view(), name='create_user'),
     url(r'^(\w+)/score/', login_required(CreateScore.as_view()), name='create_score'),
-    url(r'^(?P<pk>[0-9]+)/edition/$', login_required(UpdateScore.as_view()), name='update_score'),
+    url(r'^(?P<pk>[0-9]+)/score/edition/$', login_required(UpdateScore.as_view()), name='update_score'),
+    url(r'^(?P<pk>[0-9]+)/score/delete/$', login_required(DeleteScore.as_view()), name='delete_score'),
 ]
