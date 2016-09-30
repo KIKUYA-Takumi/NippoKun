d1b4d33fc494540ea82ae8ea05a4938e79245223
KIKUYA-Takumi <uwasanoace@yahoo.co.jp>
2016-09-30 17:01:33 +0900

    modify urls

--- NippoKun/report/urls.py
+++ NippoKun/report/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     url(r'^logout/$', logout, {'template_name': 'report/logout.html'}, name='logout'),
     url(r'^user_register/$', CreateUser.as_view(), name='create_user'),
     url(r'^(\w+)/score/', login_required(CreateScore.as_view()), name='create_score'),
-    url(r'^(\w+)/score/(?P<pk>[0-9]+)/edition/$', login_required(UpdateScore.as_view()), name='update_score'),
-    url(r'^(\w+)/score/(?P<pk>[0-9]+)/delete/$', login_required(DeleteScore.as_view()), name='delete_score'),
+    url(r'^(\w+)/edition/(?P<pk>[0-9]+)/$', login_required(UpdateScore.as_view()), name='update_score'),
+    url(r'^(\w+)/delete/(?P<pk>[0-9]+)/$', login_required(DeleteScore.as_view()), name='delete_score'),
     url(r'^(\w+)/score_list/$', login_required(ListScore.as_view()), name='list_score')
 ]
