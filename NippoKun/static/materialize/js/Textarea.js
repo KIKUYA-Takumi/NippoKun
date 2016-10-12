bbb6f6dd9c68d97a65591f8ddab58a2753695585
KIKUYA-Takumi <uwasanoace@yahoo.co.jp>
2016-10-12 14:53:15 +0900

    modify js

--- NippoKun/static/materialize/js/Textarea.js
+++ NippoKun/static/materialize/js/Textarea.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     Materialize.updateTextFields();
+    $('select').material_select();
   });
 $('#textarea1').val('New Text');
 $('#textarea1').trigger('autoresize');
