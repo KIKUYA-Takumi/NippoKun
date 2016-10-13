ecfcb0289a3adbce183275c7c33d749a413c03ab
KIKUYA-Takumi <uwasanoace@yahoo.co.jp>
2016-10-13 09:01:52 +0900

    modify setting

--- NippoKun/NippoKun/settings.py
+++ NippoKun/NippoKun/settings.py
@@ -25,8 +25,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secret.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
